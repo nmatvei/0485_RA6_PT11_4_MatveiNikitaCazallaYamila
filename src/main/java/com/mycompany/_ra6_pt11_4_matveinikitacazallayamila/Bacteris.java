9f1f6501195728956ee40775c911f9845a43b968
ycaza <ycaza@DESKTOP-T1UA6VD>
2025-04-09 08:20:00 +0200

    Prototips de mètodes principals finalitzats

--- src/main/java/com/mycompany/_ra6_pt11_4_matveinikitacazallayamila/Bacteris.java
+++ src/main/java/com/mycompany/_ra6_pt11_4_matveinikitacazallayamila/Bacteris.java
@@ -10,62 +10,146 @@ package com.mycompany._ra6_pt11_4_matveinikitacazallayamila;
  */
 public class Bacteris {
 
-    private int matriu[][];
-    private boolean coloniaEstable;
-    int numIteracions;
-    final static int DIMENSIO_COLONIA = 30;
+     /*Declaració de variables i atributs*/
+    private char colonia[][];
+    private char coloniaNova[][];
+    private char coloniaAntiga[][];
+    private boolean coloniaEstable, coloniaDuesVoltes;
+    private int numIteracions;
+    private final static int DIMENSIO_PER_DEFECTE = 30;
 
+    /**
+     * Constructor per defecte que truca al paràmetrizat
+     */
     public Bacteris() {
-        this(DIMENSIO_COLONIA);
+        this(DIMENSIO_PER_DEFECTE);
     }
 
-    public Bacteris(int dimensioColonia) {
-        this.matriu = new int[dimensioColonia][dimensioColonia];
+    /**
+     * Constructor parametritzat
+     * 
+     * @param dimensio 
+     */
+    public Bacteris(int dimensio) {
+        this.colonia = new char [dimensio][dimensio];
+        this.generacioIncial();
+        this.coloniaNova = new char[dimensio][dimensio];
+        this.coloniaAntiga = new char[dimensio][dimensio];
+        this.coloniaNova = this.colonia;
+        this.coloniaEstable = false;
+        this.coloniaDuesVoltes = false;
+        this.numIteracions = 0;
+    }
+    
+    public int getNumIteracions() {
+        return numIteracions;
     }
 
-    public void generacioInicial() {
-        for (int i = 0; i < matriu.length; i++) {
-            for (int j = 0; j < matriu[i].length; j++) {
-                matriu[i][j] = (int) (Math.random() * 2);
+    public boolean isColoniaEstable() {
+        return coloniaEstable;
+    }
+    
+    public boolean isColoniaDuesVoltes() {
+        return coloniaDuesVoltes;
+    }
+
+    /**
+     * Mètode generacioIncial per omplir la matriu que representa la colònia de 
+     * bacteris
+     */
+    private void generacioIncial() {
+        for (int i = 0; i < colonia.length; i++) {
+            for (int j = 0; j < colonia.length; j++) {
+                int nombre = (int) (Math.random() * 2);
+                if (nombre == 1){
+                    colonia[i][j] = '#';
+                } else{
+                    colonia[i][j] = ' ';
+                }
             }
         }
     }
+    
+     /**
+     * Mètode novaGeneracio per generar una nova generacio de bacteris a la colònia
+     */
+    public void novaGeneracio(){
+        
+        int comptadorEstable = 0;
+        
+        this.coloniaAntiga = colonia;
+        
+        for (int i = 0; i < colonia.length; i++) {
+            for (int j = 0; j < colonia.length; j++) {
+                int veins = veins(i,j);
+                if(colonia[i][j] == '#'){
+                    if (veins <= 1){
+                        coloniaNova[i][j] = ' ';
+                    } else if (veins == 2 || veins == 3) {
+                        comptadorEstable++;
+                    } else if (veins > 3) {
+                        coloniaNova[i][j] = ' ';
+                    }
+                } else{
+                    if (veins == 3){
+                        coloniaNova[i][j] = '#';
+                    } else {
+                        comptadorEstable++;
+                    }
+                }
+            }
+        }
+        
+        if (comptadorEstable == (colonia.length * colonia[0].length)) {
+            this.coloniaEstable = true;
+        }
+        
+        //compararColonies();
+        
+        this.numIteracions++;
+    }
 
-    public void mostrarInformacio() {
-        for (int i = 0; i < matriu.length; i++) {
+    /**
+     * Mètode per mostrar com es veu la colònia de bacteris en l'actual generàció
+     */
+    public void mostrarColonia(){
+        for (int i = 0; i < colonia.length; i++) {
             System.out.print("|");
-            for (int j = 0; j < matriu[i].length; j++) {
-                System.out.print(matriu[i][j]);
+            for (int j = 0; j < colonia.length; j++) {
+                System.out.print(colonia[i][j]);
             }
-            System.out.println("|");
+            System.out.print("|\n");
         }
     }
 
-    public void generarGeneracio() {
-
-    }
-
+    
+    /**
+     * Mètode per comptar el veïns
+     * @param posicioI posició de la fila
+     * @param posicioJ posició de la columna
+     * @return el recompte de veïns
+     */
     public int veins(int posicioI, int posicioJ) {
         
         int sumVeins = 0, iniciI = 1, iniciJ = 1 , finalI = 2, finalJ = 2;
-        final int VECINO = 1;
+        final char VEI = '#';
         
         if (posicioI == 0) {
             iniciI = 0;
-        } else if (posicioI == (matriu.length - 1)) {
+        } else if (posicioI == (colonia.length - 1)) {
             finalI = 1;
         }
         
         if (posicioJ == 0) {
             iniciJ = 0;
-        } else if (posicioJ == (matriu[posicioI].length -1)) {
+        } else if (posicioJ == (colonia[posicioI].length -1)) {
             finalJ = 1;
         }
 
         for (int i = posicioI - iniciI; i < (posicioI + finalI); i++) {
             for (int j = posicioJ - iniciJ; j < (posicioJ + finalJ); j++) {
                 if (!(i == posicioI && j == posicioJ)) {
-                    if(matriu[i][j] == VECINO){
+                    if(colonia[i][j] == VEI){
                        sumVeins++; 
                     }
                 }
@@ -74,5 +158,28 @@ public class Bacteris {
 
         return sumVeins;
     }
-
+    
+    /*
+    public void compararColonies() {
+        
+        int comptadorIguals = 0;
+        
+        for (int i = 0; i < colonia.length; i++) {
+            for (int j = 0; j < colonia[i].length; j++) {
+                if (coloniaNova[i][j] == colonia[i][j]) {
+                    comptadorIguals++;
+                } else if (coloniaNova[i][j] == coloniaAntiga[i][j]) {
+                    this.coloniaDuesVoltes = true;
+                } else {
+                    this.colonia = coloniaNova;
+                }
+            }
+        }
+        
+        if (comptadorIguals == (colonia.length * colonia[0].length)) {
+            this.coloniaEstable = true;
+        }
+        
+    }
+*/
 }
--- src/main/java/com/mycompany/_ra6_pt11_4_matveinikitacazallayamila/Principal.java
+++ src/main/java/com/mycompany/_ra6_pt11_4_matveinikitacazallayamila/Principal.java
@@ -1,5 +1,7 @@
 package com.mycompany._ra6_pt11_4_matveinikitacazallayamila;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Nikita i Yamila
@@ -8,13 +10,44 @@ public class Principal {
 
     public static void main(String[] args) {
         
-        Bacteris b = new Bacteris(3);
+        /*Declaracio de variables*/
+        Scanner sc = new Scanner(System.in);
+        String opcio;
+        Bacteris colonia;
+        int tamany;
         
-        b.generacioInicial();
+        System.out.println("DEMOSTRACIÓ DE LA CLASSE BACTERIS");
+        System.out.println("\nAbans de començar, vols canviar el tamany de la placa "
+                + "on es troben els bacteris (si no ho fas, la placa serà de 30x30)?");
+        System.out.print("Escull (S/N): ");
+        opcio = sc.next();
+        if (opcio.equalsIgnoreCase("S")){
+            System.out.println("\nQuin tamany vols per la placa?");
+            System.out.print("Introdueix el valor: ");
+            tamany = sc.nextInt();
+            colonia = new Bacteris(tamany);
+        }
+        else{
+            colonia = new Bacteris();
+        }
+        System.out.println("\nPartim desde aquest punt:");
+        colonia.mostrarColonia();
         
-        b.mostrarInformacio();
+        while(!colonia.isColoniaEstable() && !colonia.isColoniaDuesVoltes()){
+            colonia.novaGeneracio();
+            System.out.println("");
+            if (!colonia.isColoniaEstable()) {
+                colonia.mostrarColonia();
+            }
+            
+        }
         
-        b.veins(2, 2);
+        if (!colonia.isColoniaDuesVoltes()) {
+            System.out.println("La colònia és estable després de " +
+                colonia.getNumIteracions() + " d'iteracions.");
+        } else {
+            System.out.println("La colònia mai serà estable.");
+        }
         
     }
 }
